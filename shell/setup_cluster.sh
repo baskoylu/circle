cf8cfc165f159110c4dd95d3a03ec3172255c3d8
Kamal Maher <kmaher@mit.edu>
2020-03-12 17:26:22 -0400

    Corrected path to bin folder

--- shell/setup_cluster.sh
+++ shell/setup_cluster.sh
@@ -8,14 +8,14 @@ for file in ~/circle/shell/*; do # Add all shell scripts to bin
 	cp $file ~/bin/
 done
 
-# 2. Add that bin file to path
+# 2. Add that bin folder to path
 cp ~/circle/shell/.bash_profile ~/ # This file denotes the path to the bin that we will add to the user's default path
 source ~/.bash_profile # Activate changes
 
 # 3. Make folders for data and move all measureCam files there
 mkdir /om2/user/$USER/data/
 mkdir /om2/user/$USER/data/circle_data_cluster/
-for file in ~/patch/measure/*; do
+for file in ~/circle/measure/*; do
 	cp $file /om2/user/$USER/data/circle_data_cluster/
 done
 
