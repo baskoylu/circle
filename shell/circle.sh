7374edd7ce3368ecd1a68a9d22f6a8e45c9725ba
Kamal Maher <kmaher@mit.edu>
2020-03-12 15:45:01 -0400

    init commit

---
+++ shell/circle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+date=$1
+
+# Loop through .avi files and submit processing jobs
+for vidfolder in /om2/user/$USER/data/circle_data_cluster/$date/*; do
+        cp ~/circle/measure/1504_730_2304_2300.Ring.mat $vidfolder/$(basename $vidfolder).Ring.mat # copy generic ring to folder
+	sbatch ~/circle/shell/subcircle.sh $date $(basename $vidfolder)
+done
---
+++ shell/subcircle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --output=/home/%u/log/circle_%j.txt
+#SBATCH -n1
+#SBATCH --job-name=circle
+#SBATCH --nodes=1
+#SBATCH --mem=40000
+#SBATCH --time=100:00:00
+
+# $1: Name of video
+date=$1
+vidname=$2
+
+# Load matlab and launch tracker command.
+module add mit/matlab/2014a
+matlab -nodisplay -r "addpath(genpath('/home/$USER/circle')), addpath(genpath('/om2/user/$USER/data/circle_data_cluster')), TrackerAutomatedScript2('/om2/user/$USER/data/circle_data_cluster/$date/$vidname/${vidname}.avi', 'scale', '/home/$USER/circle/measure/measureCam.avi', 'NumWorms', [30, 80]), exit;"
