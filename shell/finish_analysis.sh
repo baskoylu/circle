5c2fe4806dde86a50272960ed9f056f510aba97c
Kamal Maher <kmaher@mit.edu>
2020-03-16 14:39:01 -0400

    Generalized paths

--- shell/finish_analysis.sh
+++ shell/finish_analysis.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-date=$1
+user=$1
+date=$2
 
-for folder in /mnt/x/circle_data_local/$date/*; do
-	prefix=${folder##*/}
-	scp $USER@openmind.mit.edu:/om2/user/$USER/data/circle_data_cluster/$date/$prefix/${prefix}.linkedTracks.mat $folder
-done
+echo "Copy files from cluster local drive? (y/n)"
+PROCEED="n"
+read PROCEED
+if [ $PROCEED == "y" ]; then
+        echo "Copying .linkedTracks and .stim files to local G:/ drive."
+        for folder in /mnt/x/circle_data_local/$date/*; do
+                prefix=${folder##*/}
+                scp $USER@openmind.mit.edu:/om/user/$USER/data/circle_data_cluster/$date/$prefix/${prefix}.linkedTracks.mat $folder
+                cp /mnt/c/users/$user/documents/stimfiles/$date/${prefix}.stim $folder
+        done
+fi
+
+
+echo
+
+
+echo "Copy files from local drive to server? (y/n)"
+read PROCEED
+if [ $PROCEED == "y" ]; then
+        echo "Copying .linkedTracks, .stim, and .avi files to the server."
+        ssh $user@flv-c1.mit.edu "mkdir /data1/prj_risk/data/$date/"
+        for folder in /mnt/x/circle_data_local/$date/*; do
+                scp -r $folder $user@flv-c1.mit.edu:/data1/prj_risk/data/$date/
+        done
+fi
